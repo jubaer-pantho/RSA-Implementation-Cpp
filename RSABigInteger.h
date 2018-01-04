ac390263946ee3248a88c2bec5730ce0177e3750
Md Jubaer Hossain Pantho <jubaer.pantho@gmail.com>
2018-01-04 03:27:03 -0600

    Update RSABigInteger.h

--- RSABigInteger.h
+++ RSABigInteger.h
@@ -1,25 +1,26 @@
-
-#include "BigInteger.h"
-
 #ifndef RSABIGINTEGER_H
 #define RSABIGINTEGER_H
-
+#include "BigInteger.h"
 
 class RSABigInteger
 {
     public:
-        int nSize;
-
-        RSABigInteger();
-
-        void eGeneration(BigInteger& phi,BigInteger& result);
-        void CalculateD(BigInteger& e, BigInteger& phi, BigInteger& d);
-
-        void encryption(BigInteger& PublicKey, BigInteger& N, BigInteger& msg, BigInteger& code);
-        void decryption(BigInteger& PrivateKey, BigInteger& N, BigInteger& code, BigInteger& msg);
-
+        RSABigInteger(int nSize);
         virtual ~RSABigInteger();
 
+        void CalculateD(BigInteger& e, BigInteger& phi, BigInteger& d);
+        void init(BigInteger&p, BigInteger& q);
+        void eGeneration(BigInteger& phi,BigInteger& result);
+        void encryption(BigInteger& msg, BigInteger& code);
+        void decryption(BigInteger& code, BigInteger& msg);
+        void randomNGeneration(BigInteger& randResult, int n);
+        void primeNumberGeneration(BigInteger& randPrime, int n);
+
+    private:
+        int SIZE;
+        BigInteger N;
+        BigInteger d;
+        BigInteger e;
 };
 
 #endif // RSABIGINTEGER_H
