219e1192e2e88d5217c945a5afd6fb3403ebb189
Md Jubaer Hossain Pantho <jubaer.pantho@gmail.com>
2018-01-04 03:26:31 -0600

    Update RSABigInteger.cpp

--- RSABigInteger.cpp
+++ RSABigInteger.cpp
@@ -1,21 +1,49 @@
 #include "RSABigInteger.h"
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
 
-RSABigInteger::RSABigInteger()
+RSABigInteger::RSABigInteger(int nSize)
 {
-    //ctor
+    SIZE= nSize;
+    N.setSize(nSize);
+    d.setSize(nSize);
+    e.setSize(nSize);
 }
 
 RSABigInteger::~RSABigInteger()
 {
-    //dtor
+
 }
 
+void RSABigInteger::init(BigInteger&p, BigInteger& q)
+{
+    N.multBigInteger(p,q);
+    BigInteger one(p.nSize);
+    one.digit[0] = 1;
+    BigInteger phi(p.nSize);
+
+    BigInteger tempP(p.nSize), tempQ(p.nSize);
+
+    tempP.subBigInteger(p,one);
+    tempQ.subBigInteger(q,one);
+
+    phi.multBigInteger(tempP,tempQ);
+    cout<<"\nGenerated 'phi' :"<<endl;
+    phi.showDigits();
+    eGeneration(phi,e);
+    cout<<"\nGenerated 'e' :"<<endl;
+    e.showDigits();
+    CalculateD(e,phi,d);
+    cout<<"\nGenerated 'd' :"<<endl;
+    d.showDigits();
+}
 
 void RSABigInteger::eGeneration(BigInteger& phi, BigInteger& result)
 {
     BigInteger temp(phi.nSize);
     temp.digit[0]=3;
-
     BigInteger value2(phi.nSize);
     value2.digit[0]=2;
     BigInteger temp2(phi.nSize);
@@ -29,36 +57,82 @@ void RSABigInteger::eGeneration(BigInteger& phi, BigInteger& result)
         {
             break;
         }
-
         temp2.addBigInteger(temp, value2);
-
         temp.copyBigInteger(temp2,0);
-
-
     }
-
     result.copyBigInteger(temp, 0);
 
-
-
 }
 
-void RSABigInteger::encryption(BigInteger& PublicKey, BigInteger& N, BigInteger& msg, BigInteger& code)
+void RSABigInteger::encryption(BigInteger& msg, BigInteger& code)
 {
-    BigInteger temp(N.nSize);
-
-    temp.expoModNBigInteger(msg, PublicKey, N, code);
+    BigInteger temp(SIZE);
+    temp.expoModNBigInteger(msg, e, N, code);
 
 }
 
-void RSABigInteger::decryption(BigInteger& PrivateKey, BigInteger& N, BigInteger& code, BigInteger& msg)
+void RSABigInteger::decryption(BigInteger& code, BigInteger& msg)
 {
-    BigInteger temp(N.nSize);
-
-    temp.expoModNBigInteger(code, PrivateKey, N, msg);
+    BigInteger temp(SIZE);
+    temp.expoModNBigInteger(code, d, N, msg);
 
 }
 
+void RSABigInteger::primeNumberGeneration(BigInteger& randPrime, int n)
+{
+    BigInteger valueOne(randPrime.nSize);
+    valueOne.digit[0] = 1;
+    BigInteger valueTwo(randPrime.nSize);
+    valueTwo.digit[0] = 2;
+    BigInteger valueThree(randPrime.nSize);
+    valueThree.digit[0] = 3;
+
+    BigInteger tempPrime(randPrime.nSize);
+    BigInteger tempExpoDummy(randPrime.nSize);
+    BigInteger ExpoDummy(randPrime.nSize);
+
+    BigInteger tempRemainder(randPrime.nSize);
+    BigInteger tempPrimeMinusOne(randPrime.nSize);
+    randomNGeneration(tempPrime,n);
+
+    while(1)
+    {
+        tempPrimeMinusOne.clearBigInteger();
+        tempRemainder.clearBigInteger();
+        ExpoDummy.clearBigInteger();
+        tempExpoDummy.clearBigInteger();
+        tempPrimeMinusOne.subBigInteger(tempPrime, valueOne);
+
+        ExpoDummy.expoModNBigInteger(valueTwo, tempPrimeMinusOne, tempPrime, tempRemainder);
+
+        if(tempRemainder.msbBigInteger()==0 && tempRemainder.digit[0]==1)
+        {
+            cout<<"Prime Number Generating ... Wait a little bit\n";
+            tempRemainder.clearBigInteger();
+            tempExpoDummy.expoModNBigInteger(valueThree, tempPrimeMinusOne, tempPrime, tempRemainder);
+            if(tempRemainder.msbBigInteger()==0 && tempRemainder.digit[0]==1)
+            {
+                break;
+            }
+        }
+        tempPrime.clearBigInteger();
+        randomNGeneration(tempPrime,n);
+    }
+    randPrime.copyBigInteger(tempPrime,0);
+}
+
+void RSABigInteger::randomNGeneration(BigInteger& randResult, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        unsigned int rnd = rand();
+        randResult.digit[i] = randResult.digit[i] | rnd<<16;
+        rnd = rand();
+        randResult.digit[i] = randResult.digit[i] | rnd<<1;
+    }
+    randResult.digit[0] = randResult.digit[0] | 1;
+    randResult.digit[n-1] = randResult.digit[n-1] | 1<<30;
+}
 
 void RSABigInteger::CalculateD(BigInteger& e, BigInteger& phi, BigInteger& d)
 {
